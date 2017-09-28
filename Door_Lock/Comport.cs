33c04921562360103ae65cd94f41ea9b1cb3c897
Lasse Rehhoff Holt <lass8686@elev.techcollege.dk>
2017-09-28 14:26:54 +0200

    comport class

---
+++ Door_Lock/Comport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Door_Lock
+{
+    class Comport
+    {
+        SerialPort serialPort;
+
+        public Comport()
+        {
+            serialPort = new SerialPort("COM4", 9600);
+            comPorts.Text = "Select a COMPort";
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            serialPort.Open();
+        }
+        ~Comport()
+        {
+            serialPort.Close();
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            string testString = null;
+            string indata = serialPort.ReadLine();
+
+            
+        }
+    }
+}
